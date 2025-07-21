7f2bd05dbdacf8d82ffbad3d1f9de67402c7f0e6
Saurabh Agrawal <sagrawal@rabodis.com>
2025-07-20 17:53:05 -0700

    Configure for GitHub Pages

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ghPages from 'vite-plugin-gh-pages'
 
 export default defineConfig({
   base: '/paradise-nursery/', 
-  plugins: [react(), ghPages()],
+  plugins: [react()],
 })
 
 
